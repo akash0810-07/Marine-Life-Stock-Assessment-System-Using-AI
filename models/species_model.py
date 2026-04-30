f188b9f0b95c956aeba76d07147c0035b14ce4eb
anuroopguptacsaiml24-tech <anuroop.gupta_cs.aiml24@gla.ac.in>
2026-04-30 23:50:05 +0530

    Update species_model.py

--- models/species_model.py
+++ models/species_model.py
@@ -2,8 +2,8 @@ from config import db
 
 class Species(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    population = db.Column(db.Integer, nullable=True)
-    
+    name = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+
     def __repr__(self):
-        return f'<Species {self.name}>'
+        return f"<Species {self.name}>"
